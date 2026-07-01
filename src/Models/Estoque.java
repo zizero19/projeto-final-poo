de19d73845b9c654a0721eee9ec90e59df959c7f
Luis Botega <luis62167476@alunos.sc.senac.br>
2026-07-01 20:06:45 -0300

    Classe Estoque

--- src/Models/Estoque.java
+++ src/Models/Estoque.java
@@ -1,6 +1,40 @@
 package Models;
 
 public class Estoque {
-    // TODO: Implementar estoque de produtos com base na modelagem de
-    // classes fornecida.
+
+    private Produto produto;
+    private int quantidade;
+
+    // Construtor
+    public Estoque(Produto produto, int quantidade) {
+        this.produto = produto;
+        this.quantidade = quantidade;
+    }
+
+    // Getters
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    // Setters
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    // toString()
+    @Override
+    public String toString() {
+        return "Estoque{" +
+                "produto=" + produto +
+                ", quantidade=" + quantidade +
+                '}';
+    }
 }
