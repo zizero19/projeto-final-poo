af321c07073c575a0a027640667fbbb76f6c6d37
Coruja1m <juaum0002@gmail.com>
2026-07-02 09:02:35 -0300

    Criacao de ItemPedido: getters, setters e metodos base

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
+import Models.*;
 public class App {
     public static void main(String[] args) throws Exception {
 
+
+        
     }
 }
--- src/Models/ItemPedido.java
+++ src/Models/ItemPedido.java
@@ -2,4 +2,55 @@ package Models;
 
 public class ItemPedido {
     // TODO: Implementar a classe ItemPedido com os atributos
+    private Produto produto;
+    private int quantidade;
+    private double subtotal;
+
+    public ItemPedido() {
+    }
+
+
+    public ItemPedido(Produto produto, int quantidade, double subtotal) {
+        this.produto = produto;
+        this.quantidade = quantidade;
+        this.subtotal = subtotal;
+    }
+
+
+    public Produto getProduto() {
+        return produto;
+    }
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+    public int getQuantidade() {
+        return quantidade;
+    }
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+    public double getSubtotal() {
+        // atualiza antes de retornar o subtotal, mas a função calcularSubtotal() está comentada
+        //subtotal = calcularSubtotal();
+        return subtotal;
+    }
+    public void setSubtotal(double subtotal) {
+        this.subtotal = subtotal;
+    }
+
+
+
+    public String toString(){
+        return "produto" + produto +
+        "\nquantidade: " + quantidade +
+        "\nsubtotal: " + subtotal;
+    }
+
+//função comentada para calcular o subtotal do item do pedido, mas não está sendo utilizada no momento
+   /*public double calcularSubtotal() {
+        return produto.getPreco() * quantidade;
+    } */ 
+
+    
+
 }
